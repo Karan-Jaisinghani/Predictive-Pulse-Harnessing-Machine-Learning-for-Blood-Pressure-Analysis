abd6162351510ab0dc5642a45b5b54df339552da
Karan Jaisinghani <145903609+Karan-Jaisinghani@users.noreply.github.com>
2026-03-06 01:08:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return
-render_template("index.html"
+render_template("index.html")
 app.secret_key = 'hypertension-predictor-secret-2024'
 
 # Load trained model
